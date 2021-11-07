dc3febd55eaffe8be231510eff73bea1515f3850
Vasily <lobanov.vk@gmail.com>
2021-11-07 17:41:38 +0300

    initial set of files

---
+++ config.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+
+# telegram credentials
+telegram_token = os.environ.get('telegram_bot_token')
+heroku_app_name = 'maramoika-bot'
+
+# Port given by Heroku
+port = os.environ.get('PORT')
